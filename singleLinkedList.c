2c43e7e8047128705078040ca367bc74b24e33fa
Venkateswarlu Payidimarry <vpayidimarry@isilon.com>
2018-10-25 16:56:36 +0530

    Added insert node at nth posision function.

--- singleLinkedList.c
+++ singleLinkedList.c
@@ -68,14 +68,61 @@ void reverseInIterative()
 	head = prev;		
 }
 
+
+void insertNodeToN(int pos)
+{
+	int i = 0;
+	if (head == NULL & pos != 1) 
+	{
+		printf("Enter wrong position\n");
+		return;
+	}
+
+	node_t *temp = (node_t *) malloc(sizeof(node_t));
+	temp->data = 200;
+	temp->next = NULL;
+
+	if (pos == 1) 
+	{
+		temp->next = head;
+		head = temp;
+		return;
+	}
+
+	node_t *temp1 = head;
+	for (i = 0; i < pos - 2; i++)
+	{
+		temp1 = temp1->next;
+		if (temp1 == NULL)
+		{
+			printf("Enter wrong position\n");
+			return;
+		}
+	}
+
+	temp->next = temp1->next;
+	temp1->next = temp;
+	return;
+}
+
 void main()
 {
 	head = NULL;
+	int position = 0;
 	insertToHead(10);
 	insertToHead(11);
 	insertToHead(12);
 	printf("LL after insertToHead:\n");
 	printLinkedList();
+	
+	/* Insert node in nth position of LL */
+	printf("Enter the position to insert node\n");
+	scanf("%d", &position);
+	insertNodeToN(position);
+	printf("LL after insert in %d th position:\n", position);
+	printLinkedList();
+	/*******************/
+	
 	printf("LL after recursive print:\n");
 	printLinkedListInRecursive(head);
 	printf("\n");
