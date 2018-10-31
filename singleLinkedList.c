2cc1ab21930b3251eac4bd1aa28c3d316294b401
Venkateswarlu Payidimarry <vpayidimarry@isilon.com>
2018-10-31 13:57:39 +0530

    Added SWITCH statement for options.

--- singleLinkedList.c
+++ singleLinkedList.c
@@ -186,48 +186,85 @@ void insertNodeToN(int pos)
 	return;
 }
 
+void prepareLinkedList(void)
+{
+	int entries = 0;
+	int i;
+	int temp = 0;
+	printf("Enter the no of nodes:");
+	scanf("%d", &entries);
+	for(i=0; i < entries; i++)
+	{
+		temp++;
+		insertToHead(temp);
+	}
+}
+
 void main()
 {
 	head = NULL;
 	int position = 0;
-	insertToHead(10);
-	insertToHead(11);
-	insertToHead(12);
-	printf("LL after insertToHead:\n");
-	printLinkedList();
-	
-	/* Print node from last */
-	printf("Enter the position to print from last\n");
-	scanf("%d", &position);
-	findNthNodeFromLast(position);
+	int option = 0;
 
-	/* Delete nth node from list */
-	printf("Enter the position to delete node\n");
-	scanf("%d", &position);
-	deleteNthNode(position);
-	printf("LL after delete %d th position:\n", position);
-	printLinkedList();
-	/* Insert node in nth position of LL */
-	printf("Enter the position to insert node\n");
-	scanf("%d", &position);
-	insertNodeToN(position);
-	printf("LL after insert in %d th position:\n", position);
-	printLinkedList();
-	/*******************/
-	/* Reverse LL in recursive */
-	revLinkedListInRecursive(head);
-	printf("LL after reverse recursive print:\n");
-	printLinkedListInRecursive(head);
-	printf("\n");
-	/******************/
-	
-	printf("LL after recursive print:\n");
-	printLinkedListInRecursive(head);
-	printf("\n");
-	reverseInIterative();
-	printf("LL after reversteInIterative:\n");
-	printLinkedList();
-	freeLinkedList();
-	printf("LL after clean:\n");
-	printLinkedList();
+
+	while (1) 
+	{
+		printf("1. Prepare linked list\n");
+		printf("2. Print node from last\n");
+		printf("3. Delete nth node from list\n");
+		printf("4. Insert node in  nth position\n");
+		printf("5. Reverse LL with recursive method\n");
+		printf("6. Reverse LL with iterative method\n");
+		printf("7. print linked list\n");
+		printf("8. Exit\n");
+		
+		printf ("Please select option to execute\n");
+		scanf("%d", &option);
+
+		if ( (option < 1) || (option > 8)) 
+		{
+			printf("Select proper option\n");
+			continue;
+		}
+
+
+		switch (option)
+		{
+			case 1:
+				prepareLinkedList();
+				continue;
+			case 2:
+				printf("Enter the position to print from last\n");
+				scanf("%d", &position);
+				findNthNodeFromLast(position);
+				continue;
+			case 3:
+				printf("Enter the position to delete node\n");
+				scanf("%d", &position);
+				deleteNthNode(position);
+				continue;
+			case 4:
+				printf("Enter the position to insert node\n");
+				scanf("%d", &position);
+				insertNodeToN(position);
+				continue;
+			case 5:
+				revLinkedListInRecursive(head);
+				printLinkedList();
+				continue;
+			case 6:
+				reverseInIterative();
+				printLinkedList();
+				continue;
+			case 7:
+				printLinkedList();
+				printf("\n");
+				continue;
+			case 8:
+				freeLinkedList();
+				break;
+		}
+		break;		
+
+	}
 }
